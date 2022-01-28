5b03617dc150262a6a8e0a25e0b11af0464dc77b
James Gitonga <40741293+JayExtra@users.noreply.github.com>
2022-01-28 22:05:51 +0300

    Event Fragment: events recycler adapter added
    Signed-off-by: James Gitonga <40741293+JayExtra@users.noreply.github.com>

--- app/src/main/java/com/dev/james/sayariproject/ui/events/EventsFragment.kt
+++ app/src/main/java/com/dev/james/sayariproject/ui/events/EventsFragment.kt
@@ -9,15 +9,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.AnimationUtils
+import android.view.inputmethod.InputMethodManager
+import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import com.dev.james.sayariproject.R
 import com.dev.james.sayariproject.databinding.FragmentEventsBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
 
 @AndroidEntryPoint
 class EventsFragment : Fragment() {
@@ -37,35 +41,41 @@ class EventsFragment : Fragment() {
         _binding = FragmentEventsBinding.inflate(layoutInflater, container , false)
 
         setUpUi()
-        observeSearchState()
+        collectSearchState()
+        //observeSearchState()
         return binding.root
     }
 
-    private fun observeSearchState() {
-        eventsViewModel.searchState?.observe(viewLifecycleOwner , { event ->
-            event.getContentIfNotHandled()?.let { state ->
-             if(state) {
-                 binding.apply {
-                     searchToggle.setImageResource(R.drawable.ic_round_cancel)
-                     searchToggle.toggle(true)
-                     searchToggle.setOnClickListener {
-                         eventsViewModel.updateSearchState(false)
-                     }
-                 }
-             }else{
+    private fun collectSearchState(){
+        lifecycleScope.launchWhenStarted {
+            eventsViewModel.sState.collectLatest { event ->
+                event.getContentIfNotHandled()?.let { state ->
+                    if(state) {
+                        binding.apply {
+                            searchToggle.setImageResource(R.drawable.ic_round_cancel)
+                            eventsSearchInputLayout.toggle(true)
+                            searchToggle.setOnClickListener {
+                                eventsViewModel.updateSearchState(false)
+                                hideSoftKeyBoard()
+                            }
+                        }
+                    }else{
 
-                 binding.apply {
-                     searchToggle.setImageResource(R.drawable.ic_search)
-                     searchToggle.toggle(false)
-                     searchToggle.setOnClickListener {
-                         eventsViewModel.updateSearchState(true)
-                     }
-                 }
+                        binding.apply {
+                            searchToggle.setImageResource(R.drawable.ic_search)
+                            eventsSearchInputLayout.toggle(false)
+                            searchToggle.setOnClickListener {
+                                eventsViewModel.updateSearchState(true)
+                            }
+                        }
 
-             }
+                    }
+                }
+
             }
-        })
+        }
     }
+
 
     private fun setUpUi() {
         //ui setup here
@@ -81,10 +91,6 @@ class EventsFragment : Fragment() {
         }
     }
 
-    private fun toggleSearchView() {
-
-    }
-
     //toggles the scroll up or down button
     fun View.toggle(show : Boolean){
         val transition : Transition = Slide(Gravity.TOP)
@@ -92,5 +98,14 @@ class EventsFragment : Fragment() {
         transition.addTarget(this)
         TransitionManager.beginDelayedTransition(this.parent as ViewGroup? , transition)
         this.isVisible = show
+    }
+
+    fun hideSoftKeyBoard(){
+        val imm = view?.let {
+            ContextCompat.getSystemService(
+                it.context ,
+            InputMethodManager::class.java)
+        }
+        imm?.hideSoftInputFromWindow(view?.windowToken, 0)
     }
 }
---
+++ app/src/main/java/com/dev/james/sayariproject/ui/events/adapter/EventsRecyclerAdapter.kt
@@ -0,0 +1,130 @@
+package com.dev.james.sayariproject.ui.events.adapter
+
+import android.graphics.drawable.Drawable
+import android.os.Build
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.view.isInvisible
+import androidx.core.view.isVisible
+import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
+import com.dev.james.sayariproject.R
+import com.dev.james.sayariproject.databinding.SingleEventColorBinding
+import com.dev.james.sayariproject.models.events.Events
+import java.lang.reflect.Array.get
+import java.text.SimpleDateFormat
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
+
+class EventsRecyclerAdapter : PagingDataAdapter<Events , EventsRecyclerAdapter.EventsViewHolder>(DiffCallback()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EventsViewHolder {
+        val binding = SingleEventColorBinding.inflate(LayoutInflater.from(parent.context) , parent , false)
+        return EventsViewHolder(binding)
+    }
+    override fun onBindViewHolder(holder: EventsViewHolder, position: Int) {
+        val item = getItem(position)
+        if(item!=null){
+         holder.bind(item)
+        }
+    }
+
+    inner class EventsViewHolder(
+        private val binding : SingleEventColorBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(events: Events){
+            binding.apply {
+                missionSlug.text = events.slug
+                eventDescription.text = events.description
+
+                setupDate(events , binding)
+                loadImage(events , binding)
+            }
+        }
+
+        private fun loadImage(events: Events, binding: SingleEventColorBinding) {
+            binding.apply {
+                Glide.with(binding.root)
+                    .load(events.imageUrl)
+                    .centerCrop()
+                    .placeholder(R.drawable.ic_image_placeholder)
+                    .error(R.drawable.ic_broken_image)
+                    .listener(object : RequestListener<Drawable> {
+                        override fun onLoadFailed(
+                            e: GlideException?,
+                            model: Any?,
+                            target: Target<Drawable>?,
+                            isFirstResource: Boolean
+                        ): Boolean {
+                           eventProgress.isInvisible = true
+                            return false
+                        }
+
+                        override fun onResourceReady(
+                            resource: Drawable?,
+                            model: Any?,
+                            target: Target<Drawable>?,
+                            dataSource: DataSource?,
+                            isFirstResource: Boolean
+                        ): Boolean {
+                            eventProgress.isVisible = false
+                            return false
+                        }
+                    })
+                    .into(eventImage)
+            }
+        }
+
+        private fun setupDate(events: Events, binding: SingleEventColorBinding) {
+           binding.apply {
+               if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+                   // only for OREO and newer versions
+                   val dateFormat = ZonedDateTime.parse(events.date)
+
+                   val API_TIME_STAMP_PATTERN = "dd-MM-yyyy_HH:mm:ss.SSS"
+
+                   val dateTimeFormatter : DateTimeFormatter =
+                       DateTimeFormatter.ofPattern(API_TIME_STAMP_PATTERN, Locale.ROOT)
+
+
+                   val createdDateFormatted = dateFormat.withZoneSameInstant(ZoneId.of("Africa/Nairobi"))
+
+                   // val formattedDate1 = createdDateFormatted.format(DateTimeFormatter.ofPattern(API_TIME_STAMP_PATTERN))
+
+                   val formattedDate2 = createdDateFormatted.format(dateTimeFormatter)
+
+                   Log.d("ArticlesRv", "setUpDate: $formattedDate2 ")
+
+
+                   eventDate.text = formattedDate2
+               }else {
+                   val dateFormat : SimpleDateFormat =
+                       SimpleDateFormat("dd-MM-yyyy'T'HH:mm:ss")
+                   val eDate : Date = dateFormat.parse(events.date)
+                   eventDate.text = eDate.toString()
+               }
+           }
+        }
+    }
+
+
+    class DiffCallback : DiffUtil.ItemCallback<Events>() {
+        override fun areItemsTheSame(oldItem: Events, newItem: Events): Boolean =
+            oldItem.id == newItem.id
+
+        override fun areContentsTheSame(oldItem: Events, newItem: Events): Boolean =
+            oldItem == newItem
+
+    }
+
+}
